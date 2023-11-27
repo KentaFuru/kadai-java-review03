8f82f591f6fe3b23e1f088b05138ac2a83b67a2e
KentaFuru <4jfjatk9KV@protonmail.com>
2023-11-28 00:16:13 +0900

    レッスン5課題提出

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,49 @@
+package baseball;
+
+public class BaseBallTeam {
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
+
+    double getRate() {
+            double winRate = (double)win / (win + lose);
+            return winRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    public int getDraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public void report() {
+        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + getRate() + "です。");
+    }
+}
